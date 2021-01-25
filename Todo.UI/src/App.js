92ef267f187eec46f13fd8387ac111ba18d7864d
russelpovey <russelglennpovey94@gmail.com>
2021-01-25 19:12:14 +0200

    initialized CRA for ui

---
+++ Todo.UI/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import styled from "styled-components";
+
+function App() {
+  return <Container>test</Container>;
+}
+
+function Login() {
+  return <LoginContainer>login</LoginContainer>;
+}
+
+function Main() {
+  return <MainContainer>Main</MainContainer>;
+}
+
+const MainContainer = styled.div``;
+const LoginContainer = styled.div``;
+const Container = styled.div``;
+
+export default App;
